8e2f04435fdcc7dc2fce3f7b05ee1c6761f97900
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2025-11-18 13:25:33 +0000

    KEH-1670: Update cicd pipeline setup (#28)
    * update cicd pipeline setup
    * update Makefile for running local
    * update the lambda image to latest hash

--- concourse/scripts/set_pipeline.sh
+++ concourse/scripts/set_pipeline.sh
@@ -1,27 +1,26 @@
-repo_name=${1}
+#!/bin/bash
+set -eo pipefail
+# Usage: ./set_pipeline.sh
 
-if [[ $# -gt 1 ]]; then
-    branch=${2}
-    git rev-parse --verify ${branch}
-    if [[ $? -ne 0 ]]; then
-        echo "Branch \"${branch}\" does not exist"
-        exit 1
-    fi
-else
-    branch=$(git rev-parse --abbrev-ref HEAD)
+# Define repository name
+repo_name="tech-audit-tool-api"
+
+# Always use the current branch
+branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || { echo "Failed to get branch name"; exit 1; })
+
+if ! git rev-parse --verify "${branch}" >/dev/null 2>&1; then
+    echo "Branch \"${branch}\" does not exist. Cannot set a pipeline without a valid branch."
+    exit 1
 fi
 
 if [[ ${branch} == "main" || ${branch} == "master" ]]; then
-    env="prod"
+    pipeline_name=${repo_name}
 else
-    env="dev"
+    # Remove non-alphanumeric characters and take the first 7 characters
+    sanitized_branch=$(echo "${branch}" | tr -cd '[:alnum:]' | cut -c1-7)
+    pipeline_name=${repo_name}-${sanitized_branch}
 fi
 
-if [[ ${env} == "dev" ]]; then
-    tag=$(git rev-parse HEAD)
-else
-    tag=$(git tag | tail -n 1)
-fi
-
-fly -t aws-sdp set-pipeline -c concourse/ci.yml -p ${repo_name}-${branch} -v branch=${branch} -v tag=${tag} -v env=${env}
+fly -t aws-sdp set-pipeline -c concourse/ci.yml -p ${pipeline_name}  -v branch=${branch} -v repo_name=${repo_name} -v env=dev
+echo "Pipeline \"${pipeline_name}\" has been set successfully."
 
