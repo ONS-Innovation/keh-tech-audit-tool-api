989daba90775eb83ad729493919a83153b4c5c60
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-23 12:46:47 +0000

    update: set recov window to 0 for secret deletion

--- terraform/lambda/secrets.tf
+++ terraform/lambda/secrets.tf
@@ -3,9 +3,13 @@
 resource "aws_secretsmanager_secret" "access_key" {
   name        = "${var.domain}-${var.service_subdomain}-access-key"
   description = "Access Key ID for tech audit tool API IAM user"
+  recovery_window_in_days        = 0    // Secret will be deleted immediately
+  force_overwrite_replica_secret = true // Allow overwriting the secret in case of changes
 }
 
 resource "aws_secretsmanager_secret" "secret_key" {
   name        = "${var.domain}-${var.service_subdomain}-secret-key"
   description = "Secret Access Key for tech audit tool API IAM user"
+  recovery_window_in_days        = 0    // Secret will be deleted immediately
+  force_overwrite_replica_secret = true // Allow overwriting the secret in case of changes
 }
