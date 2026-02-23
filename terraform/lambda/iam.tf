72e2cf6156cb2f0d331d84c38a506feba6e6616c
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-23 08:45:56 +0000

    update: rm dev_infra folder

--- terraform/lambda/iam.tf
+++ terraform/lambda/iam.tf
@@ -1,16 +1,6 @@
-terraform {
-  backend "s3" {
-    # Backend is selected using terraform init -backend-config=path/to/backend-<env>.tfbackend
-    # bucket         = "sdp-dev-tf-state"
-    # key            = "sdp-dev-tech-audit-tool-dev-infra/terraform.tfstate"
-    # region         = "eu-west-2"
-    # dynamodb_table = "terraform-state-lock"
-  }
-}
-
 # Managed policy for S3 access
-resource "aws_iam_policy" "s3_access_policy" {
-  name = "${var.domain}-${var.service_subdomain}-s3-access-policy"
+resource "aws_iam_policy" "s3_access" {
+  name = "${var.domain}-${var.service_subdomain}-s3-policy"
   
   policy = jsonencode({
     Version = "2012-10-17"
@@ -30,8 +20,8 @@ resource "aws_iam_policy" "s3_access_policy" {
 }
 
 # Managed policy for Secrets Manager access
-resource "aws_iam_policy" "secrets_manager_policy" {
-  name = "${var.domain}-${var.service_subdomain}-secrets-manager-policy"
+resource "aws_iam_policy" "secrets_access" {
+  name = "${var.domain}-${var.service_subdomain}-secrets-policy"
   
   policy = jsonencode({
     Version = "2012-10-17"
@@ -47,19 +37,20 @@ resource "aws_iam_policy" "secrets_manager_policy" {
 
 # IAM User Group
 resource "aws_iam_group" "group" {
-  name = "${var.domain}-${var.service_subdomain}-user-group"
+  name = "${var.domain}-${var.service_subdomain}-group"
   path = "/"
 }
 
-# Attach managed policies to user group
-resource "aws_iam_group_policy_attachment" "group_secrets_manager_policy_attachment" {
-  group = aws_iam_group.group.name
-  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+# Attach S3 policy to group
+resource "aws_iam_group_policy_attachment" "group_s3_access_attachment" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.s3_access.arn
 }
 
-resource "aws_iam_group_policy_attachment" "group_s3_access_policy_attachment" {
-  group = aws_iam_group.group.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
+# Attach Secrets Manager policy to group
+resource "aws_iam_group_policy_attachment" "group_secrets_attachment" {
+  group      = aws_iam_group.group.name
+  policy_arn = aws_iam_policy.secrets_access.arn
 }
 
 # IAM User
--- terraform/lambda/secrets.tf
+++ terraform/lambda/secrets.tf
@@ -1,4 +1,5 @@
-# Secrets for rotated IAM user access keys
+# Secrets Manager resources for IAM user access keys
+
 resource "aws_secretsmanager_secret" "access_key" {
   name        = "${var.domain}-${var.service_subdomain}-access-key"
   description = "Access Key ID for tech audit tool API IAM user"
