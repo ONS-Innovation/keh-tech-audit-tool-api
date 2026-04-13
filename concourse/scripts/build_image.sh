2d85abf72f054fc1a06f7f8e1e13288f562fc2f7
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2026-04-13 11:38:19 +0100

    update build image sh to use GH token wip

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -7,7 +7,17 @@ container_image=$(echo "$secrets" | jq -r .ecr_repository)
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
-podman build -t ${container_image}:${tag} resource-repo/aws_lambda_script/
+# Write token to a temp file and mount as a build secret (only used when needed)
+tmp_token_file="$(mktemp)"
+chmod 600 "$tmp_token_file"
+printf '%s' "${github_access_token}" > "$tmp_token_file"
+
+podman build \
+    --secret id=github_token,src="$tmp_token_file" \
+    -t ${container_image}:${tag} resource-repo/aws_lambda_script/
+
+# Remove token file after build 
+rm -f "$tmp_token_file"
 
 podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
 
